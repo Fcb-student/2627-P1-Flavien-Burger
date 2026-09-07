b4ff51f4ce4e826a81ada8c472cfa82aac792995
Fcb-student <flavienburger@gmail.com>
2026-09-07 12:03:53 +0200

    Begin van opdracht 1:naam en begin vlag

--- Week 1/sketch.js
+++ Week 1/sketch.js
@@ -1,7 +1,16 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 800);
 }
 
 function draw() {
   background(220);
+  fill(0);
+  text("1", 30, 40);
+  text("Flavien Burger", 40, 40);
+
+  fill(250, 0 ,0);
+  Rect();
+  fill(0, 0, 0);
+  rect();
+  fill(0, 0, 255)
 }
