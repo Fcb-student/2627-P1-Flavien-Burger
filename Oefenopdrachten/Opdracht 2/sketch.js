b90740a0a30e8ea2ad1d4d1174ef0de1e74336f8
Fcb-student <flavienburger@gmail.com>
2026-09-14 10:22:30 +0200

    oefenopdracht 2 12 en deels 3

--- Oefenopdrachten/Opdracht 2/sketch.js
+++ Oefenopdrachten/Opdracht 2/sketch.js
@@ -1,7 +1,15 @@
+let x = 100;
+let greeting = "Helloworld";
+
 function setup() {
   createCanvas(400, 400);
+  let greeting = "Hello world";
 }
 
 function draw() {
   background(220);
+
+  text(x,20,20);
+
+  text(greeting,20,60);
 }
