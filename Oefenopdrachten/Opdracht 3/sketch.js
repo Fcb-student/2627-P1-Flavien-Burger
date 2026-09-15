bd247e188211fcf7baea7d6ef383be7bf7e4350c
Fcb-student <flavienburger@gmail.com>
2026-09-15 15:09:36 +0200

    oefenopdracht if statement

--- Oefenopdrachten/Opdracht 3/sketch.js
+++ Oefenopdrachten/Opdracht 3/sketch.js
@@ -1,7 +1,29 @@
+let score = 65
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(400, 200);
 }
 
 function draw() {
   background(220);
+if (score > 90 && score == 100) {
+  fill(0,255,0);
+  text("uistekend",20,80);
+}
+
+if (score > 70 && score <= 89) {
+  fill(255,255,0);
+  text("Goed gedaan!",20,100);
+}
+
+if (score > 50 && score <= 69) {
+  fill(255,120,0)
+  text("voldoende",20,120);
+}
+
+if (score >= 0 && score <=49) {
+  fill(255,0,0);
+  text("onvoldoende",20,140);
+}
+
 }
