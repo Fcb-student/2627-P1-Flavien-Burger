44d16deae828b2687894aa31528ab17d9c724870
Fcb-student <flavienburger@gmail.com>
2026-09-15 15:13:47 +0200

    kleur hemel

--- Oefenopdrachten/Opdracht 3/sketch.js
+++ Oefenopdrachten/Opdracht 3/sketch.js
@@ -1,4 +1,4 @@
-let score = 65
+let score = 65 // mijn score
 
 function setup() {
   createCanvas(400, 200);
@@ -6,21 +6,23 @@ function setup() {
 
 function draw() {
   background(220);
-if (score > 90 && score == 100) {
+
+  // conditie voor uitstekend
+if (score > 90 && score <= 100) {
   fill(0,255,0);
   text("uistekend",20,80);
 }
-
+// conditie voor goed gedaan
 if (score > 70 && score <= 89) {
   fill(255,255,0);
   text("Goed gedaan!",20,100);
 }
-
+// conditie voor voldoende
 if (score > 50 && score <= 69) {
   fill(255,120,0)
   text("voldoende",20,120);
 }
-
+// conditie voor onvoldoende
 if (score >= 0 && score <=49) {
   fill(255,0,0);
   text("onvoldoende",20,140);
--- Week 2/sketch.js
+++ Week 2/sketch.js
@@ -3,11 +3,12 @@ function setup() {
 }
 
 function draw() {
-  background(80, 230, 230);
+  background(150, 255, 255);
+
   noStroke()
-  fill(200,200,0,200);
+  fill(200,200,0,200); // zonne stralling
   circle(100,60,75);
-  fill(255,255,0);
+  fill(255,255,0); // zon maken
   circle(100,60,50);
 
 }
