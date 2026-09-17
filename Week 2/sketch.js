8fcc92e2795a725e6404fb3b6a8c535adc6ccee2
Fcb-student <flavienburger@gmail.com>
2026-09-17 11:51:08 +0200

    auto's, bomen en wolken

--- Week 2/sketch.js
+++ Week 2/sketch.js
@@ -27,19 +27,31 @@ function draw() {
   triangle(750,520,750,250,850,520);
 // wolken
   fill(230);
-  circle(30,100,40);
+  circle(30,100,40); // eerste
   circle(50,110,40);
   circle(70,90,40);
-  circle(200,80,40);
+  circle(200,80,40); // tweede
   circle(220,90,40);
   circle(240,70,40);
+  circle(510,105,60); // derde
+  circle(520,120,60);
+  circle(540,135,60);
+  circle(760,105,50); // vierde
+  circle(790,90,40);
+  circle(830,100,50);
   fill(240);
-  circle(40,100,40);
+  circle(40,100,40); // eerste
   circle(60,110,40);
   circle(80,90,40);
-  circle(210,80,40);
+  circle(210,80,40); // tweede
   circle(230,90,40);
   circle(250,70,40);
+  circle(500,100,60); // derde
+  circle(520,120,60);
+  circle(530,120,60);
+  circle(750,100,50); // vierde
+  circle(790,95,40);
+  circle(830,100,50);
 
   fill(0,200,50); // grond
   rect(0,500,1000,200);
@@ -58,12 +70,62 @@ function draw() {
   rect(670,600,80,10,80);
   rect(780,600,80,10,80);
   rect(890,600,80,10,80);
+  
+  fill(128,75,6); // aparte boom achter auto's
+  rect(500,385,30,130);
+  fill(25,89,20);
+  circle(480,385,80);
+  fill(39,112,33);
+  circle(510,340,80);
+  fill(14,130,4);
+  circle(540,385,80);
 
-  fill(); // bomen stam
+  // stoplicht
+  fill(100);
+  rect(900,420,30,100);
+  rect(885,300,60,120);
+  fill(255,0,0);
+  circle(915,320,30);
+  fill(242, 124, 5);
+  circle(915,360,30);
+  fill(0,255,0);
+  circle(915,400,30);
+
+  // auto's
+  fill(255,50,50); // rood
+  rect(150,500,150,40);
+  rect(150,460,125,40);
+  fill(50,50,50);
+  circle(175,540,50);
+  circle(260,540,50);
+
+  fill(100,100,255); // blauw
+  rect(450,500,150,40);
+  rect(450,460,125,40);
+  fill(50,50,50);
+  circle(475,540,50);
+  circle(560,540,50);
+
+  fill(200,200,200);
+  rect(700,600,150,40);
+  rect(700,560,125,40);
+  fill(50,50,50);
+  circle(724,640,50)
+  circle(810,640,50);
+
+  fill(128, 75, 6); // bomen stam
+  rect(50,545,30,150);
+  rect(800,545,30,150);
   // bladeren
-  fill();
-  fill();
-  fill();
+  fill(25, 89, 20);
+  circle(40,525,80);
+  circle(780,525,80);
+  fill(39, 112, 33);
+  circle(70,480,80);
+  circle(810,480,90);
+  fill(14, 130, 4);
+  circle(100,525,80);
+  circle(840,525,80);
 
 
 
