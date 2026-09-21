3f0178644a9b52d72fbf9752352f67142c3c3c40
Fcb-student <flavienburger@gmail.com>
2026-09-21 12:27:01 +0200

    week 3 speelbord

--- Week 3/sketch.js
+++ Week 3/sketch.js
@@ -1,7 +1,27 @@
+// variabel speler en bord
+let speler1 = 1
+let Speler2 = 2
+let vak1 = 0
+let vak2 = 0
+let vak3 = 0
+let vak4 = 0
+let vak5 = 0
+let vak6 = 0
+let vak7 = 0
+let vak8 = 0
+let vak9 = 0
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(1000, 850);
 }
 
 function draw() {
-  background(220);
+  background(0,0,255); // achtergrond met kleuren voor de spelers
+  fill(255,0,0);
+  rect(0,0,500,850);
+// speelbord
+  fill(60);
+  rect(150,100,700,600,40);
+  fill(140);
+  rect(180,120,80,80,10);
 }
