dc0b675173cfcea2d140d7324b377894e88d8e28
Fcb-student <flavienburger@gmail.com>
2026-09-22 10:50:05 +0200

    Update sketch.js

--- Week 3/sketch.js
+++ Week 3/sketch.js
@@ -11,18 +11,29 @@ let vak7 = 0
 let vak8 = 0
 let vak9 = 0
 
+let achtergrondRood = 535
+
 function setup() {
   createCanvas(1000, 850);
 }
 
 function draw() {
   background(0,0,255); // achtergrond met kleuren voor de spelers
+  strokeWeight(1);
   fill(255,0,0);
-  rect(0,0,500,850);
+  rect(0,0,achtergrondRood,850);
 // speelbord
-  fill(60);
-  rect(150,100,700,600,30);
-  fill(140);
-  rect(190,130,135,125,20);
-  rect(435,130,125,125,20);
+ strokeWeight(4);
+  fill(30);
+  rect(250,175,550,500,30);
+  fill(180);
+  rect(300,205,110,110,20);
+  rect(480,205,110,110,20);
+  rect(660,205,110,110,20);
+  rect(300,365,110,110,20);
+  rect(480,365,110,110,20);
+  rect(660,365,110,110,20);
+  rect(300,525,110,110,20);
+  rect(480,525,110,110,20);
+  rect(660,525,110,110,20);
 }
