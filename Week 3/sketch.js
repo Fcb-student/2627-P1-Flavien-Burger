4338616c5f679728403bfde78306da879849f76b
Fcb-student <flavienburger@gmail.com>
2026-09-25 12:01:51 +0200

    Update sketch.js

--- Week 3/sketch.js
+++ Week 3/sketch.js
@@ -12,7 +12,6 @@ let vak7 = 0;
 let vak8 = 0;
 let vak9 = 0;
 
-
 let grijs = 0; // kleuren vakjes
 let Rood = [255, 0, 0];
 let blauw = [0, 0, 255];
@@ -23,6 +22,8 @@ let grijzeVlakX = 180;
 let grijzeVlakY = 105;
 let grijzeVlakW = 110;
 let grijzeVlakH = 110;
+
+let gameOver = false;
 
 function setup() {
   createCanvas(800, 700);
@@ -183,117 +184,133 @@ function draw() {
 
   if (vak4 == 1) {
     fill(255, 0, 0);
-    rect(grijzeVlakX, grijzeVlakY +160, 110, 110, 20);
+    rect(grijzeVlakX, grijzeVlakY + 160, 110, 110, 20);
   } else if (vak4 == 2) {
     fill(0, 0, 255);
-    rect(grijzeVlakX, grijzeVlakY +160, 110, 110, 20);
+    rect(grijzeVlakX, grijzeVlakY + 160, 110, 110, 20);
   }
 
   if (vak5 == 1) {
     fill(255, 0, 0);
-    rect(grijzeVlakX +165, grijzeVlakY +160, 110, 110, 20);
+    rect(grijzeVlakX + 165, grijzeVlakY + 160, 110, 110, 20);
   } else if (vak5 == 2) {
     fill(0, 0, 255);
-    rect(grijzeVlakX +165, grijzeVlakY +160, 110, 110, 20);
+    rect(grijzeVlakX + 165, grijzeVlakY + 160, 110, 110, 20);
   }
 
   if (vak6 == 1) {
     fill(255, 0, 0);
-    rect(grijzeVlakX + 330, grijzeVlakY +160, 110, 110, 20);
+    rect(grijzeVlakX + 330, grijzeVlakY + 160, 110, 110, 20);
   } else if (vak6 == 2) {
     fill(0, 0, 255);
-    rect(grijzeVlakX + 330, grijzeVlakY +160, 110, 110, 20);
+    rect(grijzeVlakX + 330, grijzeVlakY + 160, 110, 110, 20);
   }
 
   if (vak7 == 1) {
     fill(255, 0, 0);
-    rect(grijzeVlakX, grijzeVlakY +320, 110, 110, 20);
+    rect(grijzeVlakX, grijzeVlakY + 320, 110, 110, 20);
   } else if (vak7 == 2) {
     fill(0, 0, 255);
-    rect(grijzeVlakX, grijzeVlakY +320, 110, 110, 20);
+    rect(grijzeVlakX, grijzeVlakY + 320, 110, 110, 20);
   }
 
   if (vak8 == 1) {
     fill(255, 0, 0);
-    rect(grijzeVlakX +165, grijzeVlakY +320, 110, 110, 20);
+    rect(grijzeVlakX + 165, grijzeVlakY + 320, 110, 110, 20);
   } else if (vak8 == 2) {
     fill(0, 0, 255);
-    rect(grijzeVlakX +165, grijzeVlakY +320, 110, 110, 20);
+    rect(grijzeVlakX + 165, grijzeVlakY + 320, 110, 110, 20);
   }
 
   if (vak9 == 1) {
     fill(255, 0, 0);
-    rect(grijzeVlakX +330, grijzeVlakY +320, 110, 110, 20);
+    rect(grijzeVlakX + 330, grijzeVlakY + 320, 110, 110, 20);
   } else if (vak9 == 2) {
     fill(0, 0, 255);
-    rect(grijzeVlakX + 330, grijzeVlakY +320, 110, 110, 20);
+    rect(grijzeVlakX + 330, grijzeVlakY + 320, 110, 110, 20);
   }
 
   checkForWin();
 
+  showResetButton();
 }
 
-function checkForWin()
-{
-  if ( vak1 == vak2 && vak2 == vak3 && vak1 != 0)
-  {
-    fill(0,255,0);
-    text("Winnaar", 375,95);
-    stroke(0,255,0);
-    line(200,160,600,160);
+function showResetButton() {
+  if (gameOver == true) {
+    fill(0);
+    rect(300, 600, 200, 30);
+    text("reset", 310, 210);
   }
-  else if( vak4 == vak5 && vak5 == vak6 && vak4 != 0)
-  {
-    fill(0,255,0);
-    text("Winnaar", 375,95);
-    stroke(0,255,0);
-    line(200,320,600,320);
-  }
-  else if( vak7 == vak8 && vak8 == vak9 && vak7 != 0)
-  {
-    fill(0,255,0);
-    text("Winnaar", 375,95);
-    stroke(0,255,0);
-    line(200,480,600,480);
-  }
-  else if( vak1 == vak4 && vak4 == vak7 && vak1 != 0)
-  {
-    fill(0,255,0);
-    text("Winnaar", 375,95);
-    stroke(0,255,0);
-    line(230,120,230,510);
-  }
-  else if( vak2 == vak5 && vak5 == vak8 && vak2 != 0)
-  {
-    fill(0,255,0);
-    text("Winnaar", 375,95);
-    stroke(0,255,0);
-    line(400,120,400,510);
-  }
-  else if( vak3 == vak6 && vak6 == vak9 && vak3 != 0)
-  {
-    fill(0,255,0);
-    text("Winnaar", 375,95);
-    stroke(0,255,0);
-    line(560,120,560,510);
-  }
-  else if( vak1 == vak5 && vak5 == vak9 && vak1 != 0)
-  {
-    fill(0,255,0);
-    text("Winnaar", 375,95);
-    stroke(0,255,0);
-    line(200,120,590,510);
-  }
-   else if( vak7 == vak5 && vak5 == vak3 && vak7 != 0)
-  {
-    fill(0,255,0);
-    text("Winnaar", 375,95);
-    stroke(0,255,0);
-    line(590,120,200,510);
+}
+
+function checkForWin() {
+  // als drie vakje sop een rij staat dan komt er winnaar met een lijn over de vakjes
+  if (vak1 == vak2 && vak2 == vak3 && vak1 != 0) {
+    fill(0, 255, 0);
+    text("Winnaar", 375, 95);
+    gameOver = true;
+    stroke(0, 255, 0);
+    line(200, 160, 600, 160);
+  } else if (vak4 == vak5 && vak5 == vak6 && vak4 != 0) {
+    fill(0, 255, 0);
+    text("Winnaar", 375, 95);
+    gameOver = true;
+    stroke(0, 255, 0);
+    line(200, 320, 600, 320);
+  } else if (vak7 == vak8 && vak8 == vak9 && vak7 != 0) {
+    fill(0, 255, 0);
+    text("Winnaar", 375, 95);
+    gameOver = true;
+    stroke(0, 255, 0);
+    line(200, 480, 600, 480);
+  } else if (vak1 == vak4 && vak4 == vak7 && vak1 != 0) {
+    fill(0, 255, 0);
+    text("Winnaar", 375, 95);
+    gameOver = true;
+    stroke(0, 255, 0);
+    line(230, 120, 230, 510);
+  } else if (vak2 == vak5 && vak5 == vak8 && vak2 != 0) {
+    fill(0, 255, 0);
+    text("Winnaar", 375, 95);
+    gameOver = true;
+    stroke(0, 255, 0);
+    line(400, 120, 400, 510);
+  } else if (vak3 == vak6 && vak6 == vak9 && vak3 != 0) {
+    fill(0, 255, 0);
+    text("Winnaar", 375, 95);
+    gameOver = true;
+    stroke(0, 255, 0);
+    line(560, 120, 560, 510);
+  } else if (vak1 == vak5 && vak5 == vak9 && vak1 != 0) {
+    fill(0, 255, 0);
+    text("Winnaar", 375, 95);
+    gameOver = true;
+    stroke(0, 255, 0);
+    line(200, 120, 590, 510);
+  } else if (vak7 == vak5 && vak5 == vak3 && vak7 != 0) {
+    fill(0, 255, 0);
+    text("Winnaar", 375, 95);
+    gameOver = true;
+    stroke(0, 255, 0);
+    line(590, 120, 200, 510);
   }
 }
 
 function mouseClicked() {
+  if (gameOver == true) {
+    rect(300, 600, 200, 30);
+
+    if (
+      mouseX > 300 &&
+      mouseX < 300 + 200 &&
+      mouseY > 600 &&
+      mouseY < 600 + 30
+    ) {
+      resetGame();
+    }
+    return; //springt weg uit de functie, alles hieronder wordt niet meer uitgevoerd.
+  }
+
   // de vakje veranderd door het click van een muis en de speler verwisseld
   if (
     mouseX > grijzeVlakX &&
@@ -326,10 +343,10 @@ function mouseClicked() {
   }
 
   if (
-    mouseX > grijzeVlakX&&
-    mouseX < grijzeVlakX+ grijzeVlakW &&
-    mouseY > grijzeVlakY +160 &&
-    mouseY < grijzeVlakY +160 + grijzeVlakH
+    mouseX > grijzeVlakX &&
+    mouseX < grijzeVlakX + grijzeVlakW &&
+    mouseY > grijzeVlakY + 160 &&
+    mouseY < grijzeVlakY + 160 + grijzeVlakH
   ) {
     vak4 = speler;
     changeTurn();
@@ -338,8 +355,8 @@ function mouseClicked() {
   if (
     mouseX > grijzeVlakX + 165 &&
     mouseX < grijzeVlakX + 165 + grijzeVlakW &&
-    mouseY > grijzeVlakY +160 &&
-    mouseY < grijzeVlakY +160 + grijzeVlakH
+    mouseY > grijzeVlakY + 160 &&
+    mouseY < grijzeVlakY + 160 + grijzeVlakH
   ) {
     vak5 = speler;
     changeTurn();
@@ -348,8 +365,8 @@ function mouseClicked() {
   if (
     mouseX > grijzeVlakX + 330 &&
     mouseX < grijzeVlakX + 330 + grijzeVlakW &&
-    mouseY > grijzeVlakY +160 &&
-    mouseY < grijzeVlakY +160 + grijzeVlakH
+    mouseY > grijzeVlakY + 160 &&
+    mouseY < grijzeVlakY + 160 + grijzeVlakH
   ) {
     vak6 = speler;
     changeTurn();
@@ -358,18 +375,18 @@ function mouseClicked() {
   if (
     mouseX > grijzeVlakX &&
     mouseX < grijzeVlakX + grijzeVlakW &&
-    mouseY > grijzeVlakY +320 &&
-    mouseY < grijzeVlakY +320 + grijzeVlakH
+    mouseY > grijzeVlakY + 320 &&
+    mouseY < grijzeVlakY + 320 + grijzeVlakH
   ) {
     vak7 = speler;
     changeTurn();
   }
 
   if (
-    mouseX > grijzeVlakX +165 &&
+    mouseX > grijzeVlakX + 165 &&
     mouseX < grijzeVlakX + 165 + grijzeVlakW &&
-    mouseY > grijzeVlakY +320 &&
-    mouseY < grijzeVlakY +320 + grijzeVlakH
+    mouseY > grijzeVlakY + 320 &&
+    mouseY < grijzeVlakY + 320 + grijzeVlakH
   ) {
     vak8 = speler;
     changeTurn();
@@ -378,8 +395,8 @@ function mouseClicked() {
   if (
     mouseX > grijzeVlakX + 330 &&
     mouseX < grijzeVlakX + 330 + grijzeVlakW &&
-    mouseY > grijzeVlakY +320 &&
-    mouseY < grijzeVlakY +320 + grijzeVlakH
+    mouseY > grijzeVlakY + 320 &&
+    mouseY < grijzeVlakY + 320 + grijzeVlakH
   ) {
     vak9 = speler;
     changeTurn();
@@ -392,4 +409,18 @@ function changeTurn() {
   if (speler > 2) {
     speler = 1;
   }
+}
+
+function resetGame() {
+  vak1 = 0;
+  vak2 = 0;
+  vak3 = 0;
+  vak4 = 0;
+  vak5 = 0;
+  vak6 = 0;
+  vak7 = 0;
+  vak8 = 0;
+  vak9 = 0;
+  gameOver = false;
+  speler = 1;
 }
